12d48d2ac78a25c029a668d7caf10767c4fdeb55
Bangrejas <aviefrejas@gmail.com>
2018-12-21 10:34:19 +0700

    golang

---
+++ hello.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+func main() {
+	var absen int
+	var kelas int
+	absen = 8
+	kelas = 11
+
+	str := "Avief Reja Satria"
+	fmt.Println(str)
+	fmt.Printf("Nomor Absen : %d" , absen)
+	fmt.Println()
+	fmt.Printf("Kelas : %d" , kelas)
+}
